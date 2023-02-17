151246e560639ba8b1147baef7dba9faddf3ee7a
Baizel Mathew <bmathew@financialforce.com>
2023-02-17 10:24:14 +0000

    ci: add tests for Git.ts

---
+++ test/Git/Git.spec.ts
@@ -0,0 +1,229 @@
+// import { getDefaultBranchDiff } from '../../src/FilesChanged/BranchChanges';
+import { SimpleGit } from 'simple-git';
+import { Git } from '../../src/Git/Git';
+import { TempRepoManager } from '../repo-setup/TempRepoManager';
+
+describe('Git', () => {
+  const repoManager = TempRepoManager.getInstance('./test/repos');
+
+  beforeEach(async () => {
+    await repoManager.init();
+  });
+
+  afterEach(async () => {
+    await repoManager.tearDown();
+  });
+
+  describe('version check', () => {
+    it('does not fail when Git version is equal to 2.37.0', async () => {
+      //Given/When
+      const mock = {
+        version: jest.fn().mockReturnValue({ major: 2, minor: 37, patch: 0 }),
+      } as unknown as SimpleGit;
+
+      await expect(Git.versionCheck(mock)).resolves;
+    });
+    it('fails when Git major version is lower', async () => {
+      //Given/When
+      const mock = {
+        version: jest.fn().mockReturnValue({ major: 1 }),
+      } as unknown as SimpleGit;
+
+      //Then
+      await expect(Git.versionCheck(mock)).rejects.toThrow(
+        Error('Unsupported version of git. Min version must be 2.37.0')
+      );
+    });
+
+    it('fails when Git minor version is lower', async () => {
+      //Given/When
+      const mock = {
+        version: jest.fn().mockReturnValue({ major: 3, minor: 36 }),
+      } as unknown as SimpleGit;
+
+      //Then
+      await expect(Git.versionCheck(mock)).rejects.toThrow(
+        Error('Unsupported version of git. Min version must be 2.37.0')
+      );
+    });
+    it('fails when Git patch version is more than 0', async () => {
+      //Given/When
+      const mock = {
+        version: jest.fn().mockReturnValue({ major: 3, minor: 38, patch: 1 }),
+      } as unknown as SimpleGit;
+      //Then
+      await expect(Git.versionCheck(mock)).resolves;
+    });
+  });
+
+  describe('branch operation', () => {
+    it('fails to find default branch name when head is not set', async () => {
+      //Given/When/Then
+      await expect(
+        new Git(repoManager.repoDir).getDefaultBranchName()
+      ).rejects.toThrow(
+        Error(
+          "Failed to find symbolic ref no remote HEAD with message: 'fatal: ref refs/remotes/origin/HEAD is not a symbolic ref'"
+        )
+      );
+    });
+
+    it('find default branch name when head is set', async () => {
+      //Given
+      await repoManager
+        .createOrUpdateFile('file.txt', 'Test Text')
+        .then(() => repoManager.stageAndCommitAll())
+        .then(() => repoManager.push())
+        .then(() => repoManager.setHead());
+
+      //When
+      const branchName = await new Git(
+        repoManager.repoDir
+      ).getDefaultBranchName();
+
+      //Then
+      expect(branchName).toBe('origin/main');
+    });
+
+    it('find default branch name when head is set', async () => {
+      //Given
+      await repoManager
+        .createOrUpdateFile('file.txt', 'Test Text')
+        .then(() => repoManager.stageAndCommitAll())
+        .then(() => repoManager.push())
+        .then(() => repoManager.setHead());
+
+      //When
+      const branchName = await new Git(
+        repoManager.repoDir
+      ).getDefaultBranchName();
+
+      //Then
+      expect(branchName).toBe('origin/main');
+    });
+  });
+
+  describe('file change operations', () => {
+    beforeEach(async () => {
+      await repoManager
+        .createOrUpdateFile('file.txt', 'text')
+        .then(() => repoManager.stageAndCommitAll())
+        .then(() => repoManager.push())
+        .then(() => repoManager.setHead());
+    });
+    describe('local changes', () => {
+      it('finds modified files', async () => {
+        //Given
+        await repoManager.createOrUpdateFile('file.txt', 'modify');
+        //When
+        const files = await new Git(
+          repoManager.repoDir
+        ).getLocalChangedAndCreated();
+        //Then
+        expect(files).toEqual(new Set(['file.txt']));
+      });
+
+      it('finds unstaged renamed files', async () => {
+        //Given
+        repoManager.renameFileInRepo('file.txt', 'renamed.txt');
+        // await repoManager.gitStage();
+        //When
+        const files = await new Git(
+          repoManager.repoDir
+        ).getLocalChangedAndCreated();
+        //Then
+        expect(files).toEqual(new Set(['renamed.txt']));
+      });
+
+      it('finds unstaged new files', async () => {
+        //Given
+        repoManager.createOrUpdateFile('newFile.txt', 'content');
+        //When
+        const files = await new Git(
+          repoManager.repoDir
+        ).getLocalChangedAndCreated();
+        //Then
+        expect(files).toEqual(new Set(['newFile.txt']));
+      });
+
+      it('does not include deleted files', async () => {
+        //Given
+        repoManager.rmFile('file.txt');
+        //When
+        const files = await new Git(
+          repoManager.repoDir
+        ).getLocalChangedAndCreated();
+        //Then
+        expect(files).toEqual(new Set());
+      });
+
+      it('finds staged renamed, modified and new files ', async () => {
+        //Setup
+        await repoManager
+          .createOrUpdateFile('second.txt', 'txt')
+          .then(() => repoManager.stageAndCommitAll());
+
+        //Given
+        await repoManager
+          .createOrUpdateFile('file.txt', 'modifiy')
+          .then(() => repoManager.renameFileInRepo('second.txt', 'renamed.txt'))
+          .then(() => repoManager.createOrUpdateFile('newFile.txt', 'content'))
+          .then(() => repoManager.stageAll());
+
+        //When
+        const files = await new Git(
+          repoManager.repoDir
+        ).getLocalChangedAndCreated();
+
+        //Then
+        expect(files).toEqual(
+          new Set(['file.txt', 'renamed.txt', 'newFile.txt'])
+        );
+      });
+    });
+
+    describe('branch diff changes', () => {
+      beforeEach(async () => {
+        await repoManager
+          .checkout('dev', 'main')
+          .then(() => repoManager.createOrUpdateFile('newFile.txt', 'text'))
+          .then(() => repoManager.stageAndCommitAll());
+      });
+      it('finds diff against for default branch and HEAD', async () => {
+        //Given
+        await repoManager
+          .createOrUpdateFile('anotherFileForCommit.txt', 'text')
+          .then(() => repoManager.stageAndCommitAll());
+
+        //When
+        const files = await new Git(repoManager.repoDir).diffRange(
+          'origin/main',
+          'HEAD'
+        );
+
+        //Then
+        expect(files).toEqual(
+          new Set(['newFile.txt', 'anotherFileForCommit.txt'])
+        );
+      });
+
+      it('finds diff against default branch and commit ref', async () => {
+        //Given
+        const currentRef = (await repoManager.getGitLog())[0].hash;
+
+        await repoManager
+          .createOrUpdateFile('anotherFileForCommit.txt', 'text')
+          .then(() => repoManager.stageAndCommitAll());
+
+        //When
+        const files = await new Git(repoManager.repoDir).diffRange(
+          'origin/main',
+          currentRef
+        );
+
+        //Then
+        expect(files).toEqual(new Set(['newFile.txt']));
+      });
+    });
+  });
+});
---
+++ test/repo-setup/TempRepoManager.ts
@@ -0,0 +1,108 @@
+import simpleGit, { SimpleGit } from 'simple-git';
+import fs from 'fs';
+import path from 'path';
+
+export class TempRepoManager {
+  private isInit = false;
+  private rootDir: string;
+  private remoteRepoDir: string;
+  private testRepoDir: string;
+  private static instance: TempRepoManager | undefined = undefined;
+
+  private constructor(rootDir: string) {
+    this.rootDir = rootDir;
+    this.remoteRepoDir = rootDir + '/remote.git';
+    this.testRepoDir = rootDir + '/test-repo';
+  }
+
+  public static getInstance(rootDir: string) {
+    if (!this.instance) this.instance = new TempRepoManager(rootDir);
+    return this.instance;
+  }
+  private get git(): SimpleGit {
+    if (!this.isInit) {
+      throw new Error('Cannot perform git operation before calling init()');
+    }
+    return simpleGit(this.testRepoDir);
+  }
+
+  private async initRemoteRepo() {
+    if (!fs.existsSync(this.remoteRepoDir)) {
+      fs.mkdirSync(this.remoteRepoDir, { recursive: true });
+      const git: SimpleGit = simpleGit(this.remoteRepoDir);
+      await git.init(true);
+    }
+  }
+
+  private async cloneRepo() {
+    if (fs.existsSync(this.remoteRepoDir) && !fs.existsSync(this.testRepoDir)) {
+      await simpleGit().clone(this.remoteRepoDir, this.testRepoDir);
+    }
+  }
+
+  get repoDir() {
+    return this.testRepoDir;
+  }
+
+  async init() {
+    await this.initRemoteRepo();
+    await this.cloneRepo();
+    this.isInit = true;
+  }
+
+  async setHead() {
+    return await this.git.remote(['set-head', '--auto', 'origin']);
+  }
+
+  async push() {
+    return this.git.push('origin', 'main', ['-u']);
+  }
+
+  async checkout(branchName: string, from: string) {
+    return this.git.checkoutBranch(branchName, from);
+  }
+
+  async stageAll(files: string[] = ['.']) {
+    return this.git.add(files);
+  }
+
+  async getGitLog() {
+    const log = await this.git.log();
+    return log.all;
+  }
+
+  async stageAndCommitAll() {
+    return this.stageAll().then(() => this.git.commit('commit message'));
+  }
+
+  async tearDown() {
+    fs.rmSync(this.rootDir, { recursive: true, force: true });
+  }
+
+  async createOrUpdateFile(fileName: string, content: string) {
+    const fPath = path.join(this.testRepoDir, fileName);
+    if (fs.existsSync(fPath)) {
+      fs.appendFileSync(fPath, content);
+    } else {
+      fs.writeFileSync(fPath, content);
+    }
+  }
+
+  async cleanRepo() {
+    fs.readdirSync(this.testRepoDir)
+      .filter(x => !x.startsWith('.')) //ignore Dot files and dirs
+      .forEach(file => {
+        this.rmFile(file);
+      });
+  }
+
+  rmFile(file: string) {
+    fs.unlinkSync(path.join(this.testRepoDir, file));
+  }
+
+  renameFileInRepo(from: string, to: string) {
+    const fromPath = path.join(this.testRepoDir, from);
+    const toPath = path.join(this.testRepoDir, to);
+    fs.renameSync(fromPath, toPath);
+  }
+}
