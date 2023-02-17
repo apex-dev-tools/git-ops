b931d0428de6779d9209f4aadde36d8d0223bce3
Baizel Mathew <bmathew@financialforce.com>
2023-02-17 09:32:01 +0000

    refactor: update use of git

--- src/FilesChanged/BranchChanges.ts
+++ src/FilesChanged/BranchChanges.ts
@@ -2,14 +2,14 @@
  * Copyright (c) 2022, FinancialForce.com, inc. All rights reserved.
  */
 
-import { Git } from './Git/Git';
+import { Git } from '../Git/Git';
 
 export async function getDefaultBranchDiff(dir: string): Promise<Set<string>> {
   const git = new Git(dir);
   return git.getDefaultBranchName().then(async branchName => {
     const res = await Promise.all([
       git.diffRange(branchName, 'HEAD'),
-      git.getLocalChanges(),
+      git.getLocalChangedAndCreated(),
     ]);
     return new Set(res.map(s => [...s]).flat());
   });
