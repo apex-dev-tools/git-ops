ee803486ca40b9b717dce0112dc939be6a6458ea
Baizel Mathew <bmathew@certinia.com>
2023-08-07 11:07:47 +0100

    test: add tests

--- test/OrgTracking/Tracking.spec.ts
+++ test/OrgTracking/Tracking.spec.ts
@@ -1,15 +1,8 @@
 import { expect, jest } from '@jest/globals';
 import { OrgTracking } from '../../src';
 import { Org, SfProject } from '@salesforce/core';
-import { chdir, cwd } from 'process';
 
-// import { cwd, chdir } from 'process';
-
-// const mockCore = {
-//   SfProject: { getInstance: jest.fn() },
-//   Org: { create: jest.fn() },
-// };
-const mockData = [
+const mockGetStatusData = [
   {
     fullName: 'SomeComponent',
     filePath: 'path/to/file',
@@ -46,6 +39,26 @@ const mockData = [
     conflict: true,
     state: 'nondelete',
   },
+  {
+    fullName: 'NoPathComponent',
+    filePath: undefined,
+    type: 'metadata',
+    origin: 'local',
+    ignored: false,
+    conflict: false,
+    state: 'add',
+  },
+];
+
+const mockConflictsData = [
+  {
+    filenames: [
+      'path/to/file',
+      'path/to/file/3',
+      { nonString: 'object' },
+      'path/to/file/non/existent',
+    ],
+  },
 ];
 
 jest.mock('@salesforce/core', () => {
@@ -58,8 +71,12 @@ jest.mock('@salesforce/core', () => {
 const mockConnection = jest.fn() as any;
 const mockSourceTracking = {
   ensureRemoteTracking: jest.fn(),
-  getStatus: jest.fn().mockImplementation(() => Promise.resolve(mockData)),
-  getConflicts: jest.fn(),
+  getStatus: jest
+    .fn()
+    .mockImplementation(() => Promise.resolve(mockGetStatusData)),
+  getConflicts: jest
+    .fn()
+    .mockImplementation(() => Promise.resolve(mockConflictsData)),
 };
 
 jest.mock('@salesforce/source-tracking', () => {
@@ -73,7 +90,7 @@ jest.mock('@salesforce/source-tracking', () => {
 });
 
 describe('OrgTracking', () => {
-  const PROJET_DIR = 'path/to/dir';
+  const PROJECT_DIR = 'path/to/dir';
   let cwdSpy: any, chdirSpy: any;
 
   const sfCoreSpy = {
@@ -81,7 +98,7 @@ describe('OrgTracking', () => {
     Org: { create: jest.spyOn(Org, 'create') },
   };
 
-  beforeAll(() => {
+  beforeEach(() => {
     jest.mock('process');
     cwdSpy = jest
       .spyOn(process, 'cwd')
@@ -91,7 +108,7 @@ describe('OrgTracking', () => {
     });
   });
 
-  afterAll(() => {
+  afterEach(() => {
     jest.clearAllMocks();
   });
 
@@ -99,7 +116,7 @@ describe('OrgTracking', () => {
     it('returns the correct status', async () => {
       const tracking = new OrgTracking({
         connection: mockConnection,
-        projectDir: PROJET_DIR,
+        projectDir: PROJECT_DIR,
       });
 
       const res = await tracking.getLocalStatus();
@@ -107,13 +124,13 @@ describe('OrgTracking', () => {
       expect(cwdSpy).toHaveBeenCalled();
       // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
       expect(chdirSpy.mock.calls).toEqual([
-        [PROJET_DIR],
+        [PROJECT_DIR],
         ['mocked/current/working/dir'],
       ]);
       expect(sfCoreSpy.Org.create).toHaveBeenCalledWith({
         connection: mockConnection,
       });
-      expect(sfCoreSpy.SfProject.getInstance).toHaveBeenCalledWith(PROJET_DIR);
+      expect(sfCoreSpy.SfProject.getInstance).toHaveBeenCalledWith(PROJECT_DIR);
       expect(mockSourceTracking.ensureRemoteTracking).toHaveBeenCalledWith(
         true
       );
@@ -151,6 +168,84 @@ describe('OrgTracking', () => {
             path: 'path/to/file/3',
             state: ['ignore', 'conflict', 'nondelete'],
             type: 'class',
+          },
+          {
+            fullName: 'NoPathComponent',
+            path: undefined,
+            type: 'metadata',
+            origin: 'local',
+            state: ['add'],
+          },
+        ],
+        remote: [],
+      });
+    });
+
+    it('returns the correct status with conflicts', async () => {
+      //Given
+      const tracking = new OrgTracking({
+        connection: mockConnection,
+        projectDir: PROJECT_DIR,
+      });
+
+      //When
+      const res = await tracking.getLocalStatus(true);
+
+      //Then
+      expect(cwdSpy).toHaveBeenCalled();
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      expect(chdirSpy.mock.calls).toEqual([
+        [PROJECT_DIR],
+        ['mocked/current/working/dir'],
+      ]);
+      expect(sfCoreSpy.Org.create).toHaveBeenCalledWith({
+        connection: mockConnection,
+      });
+      expect(sfCoreSpy.SfProject.getInstance).toHaveBeenCalledWith(PROJECT_DIR);
+      expect(mockSourceTracking.ensureRemoteTracking).toHaveBeenCalledWith(
+        true
+      );
+      expect(mockSourceTracking.getStatus).toHaveBeenCalledWith({
+        local: true,
+        remote: false,
+      });
+      expect(mockSourceTracking.getConflicts).toBeCalled();
+      expect(res).toEqual({
+        local: [
+          {
+            fullName: 'SomeComponent',
+            origin: 'local',
+            path: 'path/to/file',
+            state: ['conflict', 'add'],
+            type: 'class',
+          },
+          {
+            fullName: 'SomeComponent1',
+            origin: 'local',
+            path: 'path/to/file/1',
+            state: ['delete'],
+            type: 'class',
+          },
+          {
+            fullName: 'SomeComponent2',
+            origin: 'local',
+            path: 'path/to/file/2',
+            state: ['ignore', 'modify'],
+            type: 'class',
+          },
+          {
+            fullName: 'SomeComponent3',
+            origin: 'local',
+            path: 'path/to/file/3',
+            state: ['ignore', 'conflict', 'nondelete'],
+            type: 'class',
+          },
+          {
+            fullName: 'NoPathComponent',
+            origin: 'local',
+            path: undefined,
+            state: ['add'],
+            type: 'metadata',
           },
         ],
         remote: [],
